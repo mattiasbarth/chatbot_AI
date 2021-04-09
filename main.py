6078994b3b25d3cee1c6095003fbf35dc78c2311
jawid <benjimortal@gmail.com>
2021-04-09 11:52:23 +0200

    some code!

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 def main():
     first_name = 'Sayed Jawid'
     last_name = 'Mortazawi'
+
     print(f'Hi, my name is {first_name} and my last name is {last_name}. '
           f'Nice too meet you!')
 
     print(f'Please introduce yourself!')
     input(f'Your first name:')
     input('Your last name:')
-
     print(f'nice to meet you.')
 
 
